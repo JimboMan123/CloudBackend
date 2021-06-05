2d3bfc175c5a3502f7098c63da16249d62340166
JimboMan123 <tauriainenjimmie@live.se>
2021-06-05 21:14:40 +0200

    removed unnecessary code

--- index.js
+++ index.js
@@ -59,9 +59,5 @@ response.end();
 
 
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on port ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`))
 
